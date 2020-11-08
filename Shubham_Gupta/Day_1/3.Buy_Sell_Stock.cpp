3a958b570db3888b10a0eb01f72c446e42065bc1
Shubham <sg61642@gmail.com>
2020-11-09 02:23:20 +0530

    Questions completed

--- Shubham_Gupta/Day_1/3.Buy_Sell_Stock.cpp
+++ Shubham_Gupta/Day_1/3.Buy_Sell_Stock.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 using namespace std;
 // leetcode 121
- int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        if(n<=1) return 0;
-        int buy_idx=-1,buy=INT_MAX,sell=0,diff=0;
-        for(int i=0;i<n;i++){
-            if(buy>prices[i]){
-                buy=prices[i];
-                buy_idx=i;
-            }
-            
-            if(i>buy_idx && prices[i]>buy){
-                sell=prices[i];
-                diff=max(diff,sell-buy);
-            }
+int maxProfit(vector<int> &prices)
+{
+    int n = prices.size();
+    if (n <= 1)
+        return 0;
+    int buy_idx = -1, buy = INT_MAX, sell = 0, diff = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (buy > prices[i])
+        {
+            buy = prices[i];
+            buy_idx = i;
         }
-        return diff;
+
+        if (i > buy_idx && prices[i] > buy)
+        {
+            sell = prices[i];
+            diff = max(diff, sell - buy);
+        }
+    }
+    return diff;
 }
 int main()
 {
