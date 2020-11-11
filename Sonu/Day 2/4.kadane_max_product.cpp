2a833a57e6bc454320fe06d3e56be7992357612b
sonu kumar gupta <sonugupta.08.07@gmail.com>
2020-11-11 06:48:50 +0530

    Update 4.kadane_max_product.cpp

--- Sonu/Day 2/4.kadane_max_product.cpp
+++ Sonu/Day 2/4.kadane_max_product.cpp
@@ -1,19 +1,20 @@
 // https://practice.geeksforgeeks.org/problems/maximum-product-subarray3604/1
 
+
 class Solution{
 public:
 
 	// Function to find maximum product subarray
 	long long maxProduct(int *arr, int n) {
 	    // code here
-	    int ans=arr[0];
-	    int maxi=ans;
-	    int mini=ans;
+	    long long ans=arr[0];
+	    long long maxi=ans;
+	    long long mini=ans;
 	    for(int i=1; i<n; i++){
 	        if(arr[i]<0)
 	            swap(maxi, mini);
-	            maxi=max(arr[i], maxi*arr[i]);
-	            mini=min(arr[i], mini*arr[i]);
+	            maxi=max((long long)arr[i], maxi*arr[i]);
+	            mini=min((long long)arr[i], (long long) mini*arr[i]);
 	            ans=max(ans, maxi);
 	    }
 	    return ans;
