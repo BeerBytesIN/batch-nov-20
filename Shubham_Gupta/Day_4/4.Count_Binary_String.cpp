053dfb4269e2c5c960d2dc9f3a179f7c78702a3a
Shubham <sg61642@gmail.com>
2020-11-15 18:57:51 +0530

    Solved questions of Day-4

--- Shubham_Gupta/Day_4/4.Count_Binary_String.cpp
+++ Shubham_Gupta/Day_4/4.Count_Binary_String.cpp
@@ -2,49 +2,57 @@
 using namespace std;
 // leetcode 696
 
-// Approach 1: DP T.C.= O(n^2), will give TLE 
-int countBinarySubstrings(string &s) {
-        int n=s.length();
-        
-        int **dp=new int*[n]();
-        for(int i=0;i<n;i++)
-            dp[i]=new int[n]();
-        
-        int count=0;
-        for(int i=n-1;i>=0;i--){
-            for(int j=i;j<n;j++){
-                if(i==j) continue;
-                
-                if(j-i==1 && s[i]!=s[j])
-                    dp[i][j]=1;
-                 
-                else if(dp[i+1][j-1]==1 &&s[i]==s[i+1] && s[j]==s[j-1])
-                    dp[i][j]=1;
-                
-                if(dp[i][j]) count++;
-            }
-        
+// Approach 1: DP T.C.= O(n^2), will give TLE
+int countBinarySubstrings(string &s)
+{
+    int n = s.length();
+
+    int **dp = new int *[n]();
+    for (int i = 0; i < n; i++)
+        dp[i] = new int[n]();
+
+    int count = 0;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = i; j < n; j++)
+        {
+            if (i == j)
+                continue;
+
+            if (j - i == 1 && s[i] != s[j])
+                dp[i][j] = 1;
+
+            else if (dp[i + 1][j - 1] == 1 && s[i] == s[i + 1] && s[j] == s[j - 1])
+                dp[i][j] = 1;
+
+            if (dp[i][j])
+                count++;
         }
-    
-        return count;
     }
 
+    return count;
+}
+
 // Approach 2: O(n)
- int countBinarySubstrings2(string &s) {
-        int n=s.length();
-        
-        int ans=0,prev=0,curr=1;
-        for(int i=1;i<n;i++){
-            if(s[i]==s[i-1]) curr++;
-            else{
-                ans+=min(prev,curr);
-                prev=curr;
-                curr=1;
-            }
+int countBinarySubstrings2(string &s)
+{
+    int n = s.length();
+
+    int ans = 0, prev = 0, curr = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if (s[i] == s[i - 1])
+            curr++;
+        else
+        {
+            ans += min(prev, curr);
+            prev = curr;
+            curr = 1;
         }
-        
-        return ans+min(prev,curr);
     }
+
+    return ans + min(prev, curr);
+}
 int main()
 {
     int t;
