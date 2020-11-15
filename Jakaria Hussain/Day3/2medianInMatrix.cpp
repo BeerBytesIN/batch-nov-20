31830037efaff2d5293eddf2433dd7af89447862
Jakaria <41715205+jakaria9001@users.noreply.github.com>
2020-11-16 01:31:32 +0530

    Update 2medianInMatrix.cpp

--- Jakaria Hussain/Day3/2medianInMatrix.cpp
+++ Jakaria Hussain/Day3/2medianInMatrix.cpp
@@ -1,4 +1,13 @@
 // Link : https://practice.geeksforgeeks.org/problems/median-in-a-row-wise-sorted-matrix1527/1
+
+/*
+Algorithm:  
+First, we find the minimum and maximum elements in the matrix. The minimum element can be easily found by comparing the first element of each row, and similarly, the maximum element can be found by comparing the last element of each row.
+Then we use binary search on our range of numbers from minimum to maximum, we find the mid of the min and max and get a count of numbers less than our mid. And accordingly change the min or max.
+For a number to be median, there should be (r*c)/2 numbers smaller than that number. So for every number, we get the count of numbers less than that by using upper_bound() in each row of the matrix, if it is less than the required count, the median must be greater than the selected number, else the median must be less than or equal to the selected number. 
+
+*/
+
 class Solution{   
 public:
     int median(vector<vector<int>> &matrix, int r, int c){
