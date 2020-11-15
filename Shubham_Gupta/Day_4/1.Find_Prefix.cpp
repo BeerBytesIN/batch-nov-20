87427597b9f39f294e988f4a8f8915ea3bcee450
Shubham <sg61642@gmail.com>
2020-11-15 16:24:50 +0530

    Solved questions of Day-4

--- Shubham_Gupta/Day_4/1.Find_Prefix.cpp
+++ Shubham_Gupta/Day_4/1.Find_Prefix.cpp
@@ -1,25 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool find(vector<string> &s,int idx,char ch){
-        for(int i=1;i<s.size();i++)
-        {
-            if(s[i][idx]!=ch) return false;
-        }
-        return true;
+bool find(vector<string> &s, int idx, char ch)
+{
+    for (int i = 1; i < s.size(); i++)
+    {
+        if (s[i][idx] != ch)
+            return false;
     }
-    string longestCommonPrefix(vector<string>& strs) {
-        int n=strs.size();
-        if(n==0) return "";
-        string ans="";
-        for(int i=0;i<strs[0].length();i++){
-            char  ch=strs[0][i];
-            if(find(strs,i,ch) || n==1) ans+=ch;
-            else return ans;
-        }
-        return ans;
+    return true;
+}
+string longestCommonPrefix(vector<string> &strs)
+{
+    int n = strs.size();
+    if (n == 0)
+        return "";
+    string ans = "";
+    for (int i = 0; i < strs[0].length(); i++)
+    {
+        char ch = strs[0][i];
+        if (find(strs, i, ch) || n == 1)
+            ans += ch;
+        else
+            return ans;
     }
-int main(){
-    vector<string> a={"flow","flio","flhj"};
-    cout<<longestCommonPrefix(a);
+    return ans;
+}
+int main()
+{
+    vector<string> a = {"flow", "flio", "flhj"};
+    cout << longestCommonPrefix(a);
     return 0;
 }
