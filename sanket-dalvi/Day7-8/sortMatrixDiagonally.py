4f1262c80ba876be8f4585354fc0d1c33fa30bc4
sank8dalvi <sanket.dalvi.ssd@gmail.com>
2020-11-26 02:40:00 +0530

    Add Sort the Matrix Diagonally

---
+++ sanket-dalvi/Day7-8/sortMatrixDiagonally.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        
+        return
+
+# Test
+print(Solution().diagonalSort([[3,3,1,1],[2,2,1,2],[1,1,1,2]]))
