13488a9783ee8771fb0289792a39afa36db3eb84
sank8dalvi <sanket.dalvi.ssd@gmail.com>
2020-11-26 22:32:48 +0530

    Add sort matrix diagonally

--- sanket-dalvi/Day7-8/sortMatrixDiagonally.py
+++ sanket-dalvi/Day7-8/sortMatrixDiagonally.py
@@ -3,8 +3,30 @@ from typing import List
 
 class Solution:
     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
-        
-        return
+        if not mat or not mat[0]:
+            return mat
+
+        def sortD(iRow, jCol):
+            i, j = iRow, jCol
+            arr = []
+            while i < len(mat) and j < len(mat[0]):
+                arr.append(mat[i][j])
+                i += 1
+                j += 1
+            arr.sort()
+            i, j = iRow, jCol
+            k = 0
+            while i < len(mat) and j < len(mat[0]):
+                mat[i][j] = arr[k]
+                i += 1
+                j += 1          
+                k += 1
+        for row in range(len(mat)):
+            sortD(row, 0)
+        for col in range(1, len(mat[0])):
+            sortD(0, col)
+        return mat
+
 
 # Test
-print(Solution().diagonalSort([[3,3,1,1],[2,2,1,2],[1,1,1,2]]))
+print(Solution().diagonalSort([[3, 3, 1, 1], [2, 2, 1, 2], [1, 1, 1, 2]]))
