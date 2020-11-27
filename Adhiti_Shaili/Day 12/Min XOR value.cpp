db384366e1441873abb19e3b494ab18e66bb86f2
Adhiti <shaili.adhiti@gmail.com>
2020-11-27 20:32:48 +0530

    Reverse bit

--- Adhiti_Shaili/Day 12/Min XOR value.cpp
+++ Adhiti_Shaili/Day 12/Min XOR value.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int Solution::findMinXor(vector<int> &A) {
+int findMinXor(vector<int> &A) {
     int maxi=INT_MAX;
     sort(A.begin(),A.end());
     for(int i=0;i<A.size()-1;i++)
