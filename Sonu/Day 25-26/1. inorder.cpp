cc7ad63e1d0a51d2e8a4712111157f85e4a0fca9
sonu kumar gupta <sonugupta.08.07@gmail.com>
2020-12-17 13:15:58 +0530

    Update 1. inorder.cpp

--- Sonu/Day 25-26/1. inorder.cpp
+++ Sonu/Day 25-26/1. inorder.cpp
@@ -18,3 +18,33 @@ public:
 };
 
 
+
+
+
+public:
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+        vector<int> ans;
+        
+        stack<TreeNode* > st;
+        
+        while(root || !st.empty()){
+            
+            while(root){
+                st.push(root);
+                root=root->left;
+            }
+            
+            root=st.top();
+            st.pop();
+            
+            ans.push_back(root->val);
+            
+            root=root->right;
+        }
+        return ans;
+    }
+};
+
+
