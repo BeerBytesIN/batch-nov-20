d97d20040263d9f9ac845017a5fb03cc8a9cd08b
sonu kumar gupta <sonugupta.08.07@gmail.com>
2020-12-17 13:35:53 +0530

    Update 2. preorder.cpp

--- Sonu/Day 25-26/2. preorder.cpp
+++ Sonu/Day 25-26/2. preorder.cpp
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/binary-tree-preorder-traversal/
+
 class Solution {
 public:
     
@@ -12,6 +14,30 @@ public:
     }
     vector<int> preorderTraversal(TreeNode* root) {
                 fun(root);
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        
+        stack<TreeNode*> st;
+        
+        while(root || !st.empty()){
+            
+            if(root){
+                ans.push_back(root->val);
+                st.push(root);
+                root=root->left;
+            }
+            else{
+                root=st.top();
+                st.pop();
+                root=root->right;
+            }
+        }
         return ans;
     }
 };
