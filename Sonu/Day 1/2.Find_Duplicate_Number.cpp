4468c35c2adaa44c89b7989f3582490a51059311
Deepanshu-test-0 <109202379+Deepanshu-test-0@users.noreply.github.com>
2022-10-26 13:10:53 +0530

    Update 2.Find_Duplicate_Number.cpp

--- Sonu/Day 1/2.Find_Duplicate_Number.cpp
+++ Sonu/Day 1/2.Find_Duplicate_Number.cpp
@@ -4,7 +4,8 @@ https://leetcode.com/problems/find-the-duplicate-number/submissions/
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        
+        // next line dummy
+        int a= 5
         for(int i=0; i<nums.size(); i++)
         {
             if(nums[abs(nums[i])-1]<0) return abs(nums[i]);
